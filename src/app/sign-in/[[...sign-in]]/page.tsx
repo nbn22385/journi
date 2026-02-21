eb70cd2c03bf09325a695e028aa31478492f98f4
nbn22385 <nbn22385@gmail.com>
2026-02-20 21:09:02 -0500

    Add shadcn theme to sign-in and sign-up pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
+import { shadcn } from "@clerk/themes";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
-      <SignIn />
+      <SignIn appearance={{ theme: shadcn }} />
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
+import { shadcn } from "@clerk/themes";
 
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
-      <SignUp />
+      <SignUp appearance={{ theme: shadcn }} />
     </div>
   );
 }
